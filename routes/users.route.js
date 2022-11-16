9a0b335310c3ed11587657aa054599dee8f3f03c
Amir <kimciwork@gmail.com>
2022-11-16 22:16:13 +0300

    editing users

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 export const usersRouter = Router();
 import { User } from "../models/user.model.js";
+import fs from "fs";
+import path from "path";
 
 const getUsers = async () => {
   const usersData = await new User().getAll();
@@ -23,4 +25,38 @@ usersRouter.get("/:name", async (req, res) => {
     layout: "userShow",
     user: user,
   });
+});
+
+usersRouter.get("/:name/edit", async (req, res) => {
+  if (req.query.allow) {
+    //req.query хранит параметры url
+    const data = await getUsers();
+    const user = data.find((user) => user.name == req.params.name);
+    res.render("edit", {
+      layout: "userEdit",
+      user: user,
+    });
+  } else {
+    res.redirect("/users");
+  }
+});
+
+usersRouter.post("/edit", async (req, res) => {
+  let users = await getUsers();
+  const user = users.find((user) => user.id == req.body.id);
+  const index = users.indexOf(user);
+  const dir = path.dirname(import.meta.url.split("///")[1]);
+  if (index !== -1) {
+    users[index].name = req.body.name;
+    users[index].password = req.body.password;
+    users = users.map((i) => JSON.stringify(i));
+    fs.writeFile(
+      path.join(dir, "..", "database", "users.json"),
+      JSON.stringify(users),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+  res.redirect("/users");
 });
